52be23c8db0d2e1fb3e7a8bb00893db9b3921dce
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-01-21 12:28:14 +1000

    added first_name, last_name User, updates devise's views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true, length: { maximum: 255 }
+
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
