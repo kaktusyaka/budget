035bf8c148bb6d35ce505719ee1e07d697cfcf5c
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-01-30 19:31:15 +1000

    moved fextures to spec directory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require "#{::Rails.root}/spec/fixtures/auth_facebook"
 
 describe User do
   it { should have_many(:authorizations).dependent(:destroy) }
@@ -9,5 +10,21 @@ describe User do
     it { should validate_presence_of(:email) }
     it { should validate_length_of(:first_name).is_at_most(255) }
     it { should validate_length_of(:last_name).is_at_most(255) }
+  end
+
+ context '#Methods' do
+    context '#from_omniauth' do
+      context '#without current_user' do
+        it 'should create user via facebook' do
+          User.from_omniauth( env["omniauth.auth"] )
+          endorsement.update_column :status, 'step_5'
+          endorsement.pay!
+          endorsement.send(:make_refund!)
+
+          expect(endorsement.refunded_at).to_not be_blank
+          expect(endorsement.refund_id).to_not be_blank
+        end
+      end
+    end
   end
 end
