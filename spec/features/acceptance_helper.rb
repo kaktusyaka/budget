7930558c944ba9fcba587d5ee9a3b44fb7cf60d4
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-02-05 09:48:41 +1000

    added methods for login to acceptance_helper.rb and extended .gitignore

--- spec/features/acceptance_helper.rb
+++ spec/features/acceptance_helper.rb
@@ -2,3 +2,13 @@ require 'rails_helper'
 
 include Warden::Test::Helpers
 Warden.test_mode!
+
+def create_logged_in_user
+  user = create(:user)
+  login(user)
+  user
+end
+
+def login(user)
+  login_as user, scope: :user
+end
