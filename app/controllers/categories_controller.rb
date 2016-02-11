554e2cf64c36f00c256cacbca99828315a562fac
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-02-11 19:01:08 +1000

    added categories/new

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,10 @@ class CategoriesController < ApplicationController
     @categories = current_user.categories
   end
 
+  def new
+    @category = Category.new
+  end
+
   def sort
     params[:category].each_with_index do |id, index|
       Category.find(id).update_column :position, index+1
