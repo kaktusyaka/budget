7a7e00ebbba550ddecdd02398f115d0925476837
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-02-14 07:54:45 +1000

    added css styles for transaction search and fixed navbar manu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  ["categories", "transactions"].each do |page|
+    define_method("#{page}_active?") do
+      [page.to_sym].include?(controller_name.to_sym)
+    end
+  end
 end
