47aad8c29e25bf2599e4a28eae211fd7b9725394
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-02-16 13:04:15 +1000

    redesigned sign up page, send confirmation instractions page, reset password page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
       [page.to_sym].include?(controller_name.to_sym)
     end
   end
+
+  def show_devise_links ()
+    links = []
+
+    if controller_name != 'sessions'
+      links << [ "Log in", new_session_path(resource_name) ]
+    end
+    if devise_mapping.recoverable? && controller_name != 'passwords' && controller_name != 'registrations'
+      links <<  [ "Forgot your password?", new_password_path(resource_name) ]
+    end
+    if controller_name != 'registrations'
+      links << [ "Register now", new_registration_path(resource_name) ]
+    end
+    if devise_mapping.confirmable? && controller_name != 'confirmations'
+      links << [ "Didn't receive confirmation instructions?", new_confirmation_path(resource_name) ]
+    end
+
+    raw links.collect { |l| link_to l.first, l.last }.join(" | ")
+  end
+
 end
