56d8fc430a0834751b1f033522bc80c88fb7043e
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-03-13 19:32:36 +1000

    added checkout page and css for payment methods

--- app/controllers/pricing_plans_controller.rb
+++ app/controllers/pricing_plans_controller.rb
@@ -2,4 +2,8 @@ class PricingPlansController < ApplicationController
   def index
     @pricing_plans = PricingPlan.all
   end
+
+  def checkout
+    @pricing_plan = PricingPlan.find(params[:id])
+  end
 end
